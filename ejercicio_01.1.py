f2c42e2fe09f7f56ad24a0a2d16e1bf9a653d12b
lendali <gacavain@gmail.com>
2026-02-24 21:46:37 -0600

    borrar archivos

--- ejercicio_01.1.py
+++
@@ -1,9 +0,0 @@
-#Aplicacion de factoriales
-
-num = int(input("Dime un número: "))
-resultado = 1
-
-for contador in range(2, num + 1):
-    resultado *= contador
-
-print("El resultado es", resultado)
--- ejercicio_07.py
+++
@@ -1,7 +0,0 @@
-#/Realizar una algoritmo que muestre la tabla de multiplicar de un número introducido por teclado.
-
-
-tabla = int(input("¿De qué número quieres mostrar la tabla de multiplicar?: "))
-
-for num in range(1, 11):
-    print(num, "*", tabla, "=", num * tabla)
